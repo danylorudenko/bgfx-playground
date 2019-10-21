548e84024d99b8d7532c03023300c604e4f37e9a
Danylo Rudenko <rudenko.dany@gmail.com>
2019-10-21 13:05:37 +0300

    Updated shader building script for current project.:

--- process_user_shaders_dx11.bat
+++ process_user_shaders_dx11.bat
@@ -1,8 +1,8 @@
-rmdir /S /Q ..\runtime\shaders
-mkdir ..\runtime\shaders\dx11\
+rmdir /S /Q .\shaders\bin
+mkdir .\shaders\bin\
 
-for /r %%V in (shaders\vertex\*.*) do ..\..\.build\win64_vs2017\bin\shadercDebug.exe --verbose -i ..\..\src --varyingdef .\shaders\varying.def.sc --platform windows --profile "vs_5_0" --type vertex -O 0 -f "%%V" -o "..\runtime\shaders\dx11\%%~nV.bin"
+for /r %%V in (shaders\vertex\*.*) do .\tools\shaderc\shaderc.exe --verbose -i .\submodules\bgfx\src --varyingdef .\shaders\varying.def.sc --platform windows --profile "vs_5_0" --type vertex -O 0 -f "%%V" -o ".\shaders\bin\%%~nV.bin"
 
-for /r %%V in (shaders\fragment\*.*) do ..\..\.build\win64_vs2017\bin\shadercDebug.exe --verbose -i ..\..\src --varyingdef .\shaders\varying.def.sc --platform windows --profile "ps_5_0" --type fragment -O 0 -f "%%V" -o "..\runtime\shaders\dx11\%%~nV.bin"
+for /r %%V in (shaders\fragment\*.*) do .\tools\shaderc\shaderc.exe --verbose -i .\submodules\bgfx\src --varyingdef .\shaders\varying.def.sc --platform windows --profile "ps_5_0" --type fragment -O 0 -f "%%V" -o ".\shaders\bin\%%~nV.bin"
 
 pause
