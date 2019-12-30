04e73d4b12292ddfac073d1fe184551d0eb9d343
Danylo Rudenko <rudenko.dany@gmail.com>
2019-12-30 23:24:17 +0200

    Changed Win32 window title.

--- src/globals.cpp
+++ src/globals.cpp
@@ -4,7 +4,7 @@ namespace pg
 {
 
 Application::Application(char const* title, std::uint32_t width, std::uint32_t height, WindowClass::WinProcHandler procHandler)
-    : m_MainWindow{ std::make_unique<Window>(GetModuleHandleA(NULL), "MyWindow", width, height, "MyWindowClass", procHandler, this) }
+    : m_MainWindow{ std::make_unique<Window>(::GetModuleHandleA(NULL), title, width, height, "MyWindowClass", procHandler, this) }
     , m_MainRenderer{ std::make_unique<gfx::Renderer>() }
     , m_MainScene{ std::make_unique<Scene>() }
 {
